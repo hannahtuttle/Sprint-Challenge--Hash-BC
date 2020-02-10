8cef1d72869e7317ea673b5f5b9be11db699cfac
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2020-02-09 19:46:29 -0800

    finished hashtable problem2, all test passing

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -19,5 +19,35 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+    for idx in range(length):
+        # print('source',tickets[idx].source)
+        # print('destination',tickets[idx].destination)
+        hash_table_insert(hashtable, tickets[idx].source, tickets[idx].destination)
+    
+    current = hash_table_retrieve(hashtable, "NONE")
+    # prev = None
+    count = 0
+    while count is not length:
+        route[count] = current
+        current = hash_table_retrieve(hashtable, current)
+        count += 1
 
+    print('route',route)
     return route
+
+
+# ticks = [
+#   Ticket("PIT", "ORD" ),
+#   Ticket("XNA", "CID" ),
+#   Ticket("SFO", "BHM" ),
+#   Ticket("FLG", "XNA" ),
+#   Ticket("NONE", "LAX"),
+#   Ticket("LAX", "SFO" ),
+#   Ticket("CID", "SLC" ),
+#   Ticket("ORD", "NONE"),
+#   Ticket("SLC", "PIT" ),
+#   Ticket("BHM", "FLG" )
+# ]
+# legs = len(ticks)
+
+# reconstruct_trip(ticks, legs)
